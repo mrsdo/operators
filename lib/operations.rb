a1dddeb217b43245afdb47f7e661f599f050c6af
Martinique <me@martidolce.com>
2020-11-11 07:42:45 -0500

    commit tree 71ce929460cb0ac545fd27dea69fcc622d27096f

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
+# frozen_string_literal: true
+
+# require pry
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+
 def unsafe?(speed)
-
+  if speed <= 40 || speed >= 60
+    true
+  else
+    false
+  end
 end
-
-
-
+unsafe?(80)
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
-	
-
-
